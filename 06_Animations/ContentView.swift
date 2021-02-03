f19521917ddfa71147a61b323c7fec8ea5b98549
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-03 15:30:54 -0800

    Initial Commit

---
+++ 06_Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  06_Animations
+//
+//  Created by Austin Roach on 2/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 06_Animations/_6_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_AnimationsApp.swift
+//  06_Animations
+//
+//  Created by Austin Roach on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct _6_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
