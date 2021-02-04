46b6c97b0fd08d10517158266299430a65a0b043
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-03 21:57:28 -0800

    *Pats hands*

--- 06_Animations/WrapUpNotes.swift
+++ 06_Animations/WrapUpNotes.swift
@@ -8,3 +8,21 @@
 import Foundation
 
 //MARK:Animation: Wrap Up
+
+//This technique project started off easier, took a few twists and turns, and progressed into more advanced animations, but I hope it's given you an idea of just how powerful - and how flexible! - SwiftUI's animation system is.
+
+//As I've said previously, animation is about both making you app look great and also adding extra meaning. So, rather than making a view disappear abruptly, can you add a transition to help the user undrstand something is changing?
+
+//Also, don't forget what it looks like to be playful in your user interface. My all-time favorite iOS animation is one that Apple ditched when they moved to iOS 7, and it was the animation for deleting passes in the Wallet app - a mettal shredder appeared and cut your pass into a dozen strips that then dropped away. It only took a fraction of a second more than the current animation, but it was beautiful and fun too!
+
+//Challenge
+
+//One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try extending this app to make sure you fully understand what's going on.
+
+//Go back to guess the Flag project and add some animation:
+
+//1. When you tap the correct flag, make it spin around 360 degrees on the y axis.
+
+//2. Make the other two buttons fade out to 25% opacity
+
+//3. And if you tap on the wrong flag? Well, that's up to you - get creative!
